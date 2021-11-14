519254c6e535bda63fec3ddc74bfa1c4d9634b31
jfwt <acornerapp@gmail.com>
2021-11-14 15:25:13 +0800

    subscription page

--- lib/screens/subscription/plan.dart
+++ lib/screens/subscription/plan.dart
@@ -6,8 +6,10 @@ class Plan {
   String trips;
   String destination;
   String hotelPackage;
+  String insurance;
   bool expanded;
+  bool selectedPlan;
   Color color;
 
-  Plan({this.planName, this.price, this.trips, this.destination, this.hotelPackage, this.color, this.expanded = false});
+  Plan({this.planName, this.price, this.trips, this.destination, this.hotelPackage, this.insurance, this.selectedPlan, this.color, this.expanded = false});
 }
